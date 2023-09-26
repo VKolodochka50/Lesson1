33fa0086cfe80eea8cb59da49279e8669053c33e
Valerych <valeriy.kolodochka@gmail.com>
2023-09-26 11:58:31 +0200

    Initial commit

---
+++ src/main/java/de/telran/lesson1/Practice1.java
@@ -0,0 +1,16 @@
+package de.telran.lesson1;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, ");
+        System.out.println("My ");
+        System.out.println("Name ");
+        System.out.println("is ");
+        System.out.println("Vasiliy ");
+        System.out.print("Hello, ");
+        System.out.print("My ");
+        System.out.print("Name ");
+        System.out.print("is ");
+        System.out.print("New Vasiliy ");
+    }
+}
