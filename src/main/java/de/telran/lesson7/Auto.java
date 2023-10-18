135299c6a502eaf60448c33ba329af1901c5b3ba
Valerych <valeriy.kolodochka@gmail.com>
2023-10-18 11:57:03 +0200

    New array (lesson 17) etc Extract class Auto, update signatures and constructor + some logic and extend printout

---
+++ src/main/java/de/telran/lesson7/Auto.java
@@ -0,0 +1,37 @@
+package de.telran.lesson7;
+
+public class Auto {
+    String model;
+    public int year;
+    int mileage;
+    String color;
+
+    Auto() {
+        model = null;
+        year = 0;
+        mileage = 0;
+    }
+
+    Auto(String argModel) {
+        model = argModel;
+    }
+
+    Auto(String argModel, int argYear) {
+        model = argModel;
+        year = argYear;
+    }
+
+    public Auto(String model, int year, int mileage) {
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+    }
+
+    void run() {
+        System.out.println(model+" год выпуска "+year+" с пробегом "+mileage+" ЕДЕТ!");
+    }
+
+    void turn() {
+        System.out.println(model+" год выпуска "+year+" с пробегом "+mileage+" ПОВЕРНУЛ!");
+    }
+}
---
+++ src/main/java/de/telran/lesson7/ConstructorSimple.java
@@ -0,0 +1,34 @@
+package de.telran.lesson7;
+
+public class ConstructorSimple {
+    public static void main(String[] args) {
+        Auto auto1 = new Auto();
+        auto1.model = "BMW";
+        auto1.year = 2023;
+        auto1.mileage = 100;
+        auto1.run();
+        auto1.turn();
+
+        Auto auto2 = new Auto();
+        auto2.model = "VW";
+        auto2.year = 2022;
+        auto2.mileage = 5;
+        auto2.run();
+
+        Auto auto3 = new Auto("Audi");
+        auto3.run();
+
+        Auto auto4 = new Auto("Porsche", 2020);
+        auto4.mileage = 3;
+        auto4.run();
+
+        Auto auto5 = new Auto("Lada", 2023, 5);
+        auto5.run();
+
+        Auto auto6 = new Auto();
+        auto6.run();
+
+
+
+    }
+}
